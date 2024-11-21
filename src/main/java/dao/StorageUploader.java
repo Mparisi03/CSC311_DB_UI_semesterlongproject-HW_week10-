e75b3cd056494f9f1ce82a25994097484e1c5f45
michelle Parisi <parimm@farmingdale.edu>
2024-11-20 19:19:10 -0500

    work from today(11/20): trying to fix commits so I can push(deleting connection string sercert)

---
+++ src/main/java/dao/StorageUploader.java
@@ -0,0 +1,25 @@
+package dao;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobClientBuilder;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+
+public class StorageUploader {
+
+    private BlobContainerClient containerClient;
+
+    public StorageUploader( ) {
+        this.containerClient = new BlobContainerClientBuilder()
+                .connectionString("")
+                .containerName("media-files")
+                .buildClient();
+    }
+
+    public void uploadFile(String filePath, String blobName) {
+        BlobClient blobClient = containerClient.getBlobClient(blobName);
+        blobClient.uploadFromFile(filePath);
+    }
+    public BlobContainerClient getContainerClient(){
+        return containerClient;
+    }
+}
